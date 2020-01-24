9c25dfb0b8bfea1588ebc81c5dbbf5a9110aea6d
Minoru Osuka <minoru.osuka@gmail.com>
2020-01-24 23:26:37 +0900

    Add mokuzu command (#10)

---
+++ Makefile
@@ -0,0 +1,21 @@
+BIN_DIR ?= $(CURDIR)/bin
+VERSION ?=
+
+ifeq ($(VERSION),)
+  VERSION = $(shell cargo metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.name=="mokuzu") | .version')
+endif
+
+clean:
+	cargo clean
+
+format:
+	cargo fmt
+
+build:
+	cargo build --release
+
+test:
+	cargo test
+
+doc:
+	cargo doc
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, AppSettings};
+use std::io;
+
+use mokuzu::Tokenizer;
+
+fn main() {
+    let app = App::new(crate_name!())
+        .setting(AppSettings::DeriveDisplayOrder)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .help_message("Prints help information.")
+        .version_message("Prints version information.")
+        .version_short("v");
+
+    let _matches = app.get_matches();
+
+    let mut tokenizer = Tokenizer::normal();
+    loop {
+        let mut text = String::new();
+        io::stdin().read_line(&mut text).expect("Failed to read line");
+        text = text.trim().to_string();
+
+        for token in tokenizer.tokenize(&text) {
+            println!("{}\t{}", token.text,token.detail.reading);
+        }
+        println!("EOS")
+    }
+}
