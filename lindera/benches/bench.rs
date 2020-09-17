018109b8f785777048857f1398ca1a9dae896f68
Jun Ohtani <johtani@gmail.com>
2020-09-17 22:30:56 +0900

    Use new method instead of default_normal (#72)
    Closes #71

--- lindera/benches/bench.rs
+++ lindera/benches/bench.rs
@@ -5,7 +5,7 @@ use lindera::tokenizer::Tokenizer;
 
 fn bench_tokenize(c: &mut Criterion) {
     c.bench_function("bench-wiki", |b| {
-        let mut tokenizer = Tokenizer::default_normal();
+        let mut tokenizer = Tokenizer::new("normal", "");
         b.iter(|| tokenizer.tokenize("検索エンジン（けんさくエンジン、英語: search engine）は、狭義にはインターネットに存在する情報（ウェブページ、ウェブサイト、画像ファイル、ネットニュースなど）を検索する機能およびそのプログラム。"))
     });
 }
