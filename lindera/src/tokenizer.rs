ce18ebd8df7ac21a854d04a820ed949bbddd732b
abetomo <abe@enzou.tokyo>
2021-07-30 15:22:13 +0900

    test: add a test with a mix of simple and detailed user dictionary
    changed the test name and CSV file name to match the type of user dictionary described in the README.

--- lindera/src/tokenizer.rs
+++ lindera/src/tokenizer.rs
@@ -424,7 +424,7 @@ mod tests {
     }
 
     #[test]
-    fn test_user_dict() {
+    fn test_simple_user_dict() {
         let mut tokenizer = Tokenizer::new_with_userdic(Mode::Normal, "", "resources/userdic.csv");
         assert!(tokenizer.user_dict.is_some());
         assert!(tokenizer.user_dict_words_idx_data.is_some());
@@ -461,9 +461,9 @@ mod tests {
     }
 
     #[test]
-    fn test_user_dict_with_cost() {
+    fn test_detailed_user_dict() {
         let mut tokenizer =
-            Tokenizer::new_with_userdic(Mode::Normal, "", "resources/userdic_with_cost.csv");
+            Tokenizer::new_with_userdic(Mode::Normal, "", "resources/detailed_userdic.csv");
         assert!(tokenizer.user_dict.is_some());
         assert!(tokenizer.user_dict_words_idx_data.is_some());
         assert!(tokenizer.user_dict_words_data.is_some());
@@ -484,6 +484,61 @@ mod tests {
             ],
             tokens[0].detail
         );
+        let token_texts: Vec<&str> = tokens.iter().map(|token| token.text).collect();
+        assert_eq!(
+            vec![
+                "東京スカイツリー",
+                "の",
+                "最寄り駅",
+                "は",
+                "とうきょうスカイツリー駅",
+                "です"
+            ],
+            token_texts
+        );
+    }
+
+    #[test]
+    fn test_mixed_user_dict() {
+        let mut tokenizer =
+            Tokenizer::new_with_userdic(Mode::Normal, "", "resources/mixed_userdic.csv");
+        assert!(tokenizer.user_dict.is_some());
+        assert!(tokenizer.user_dict_words_idx_data.is_some());
+        assert!(tokenizer.user_dict_words_data.is_some());
+
+        let tokens: Vec<Token> =
+            tokenizer.tokenize("東京スカイツリーの最寄り駅はとうきょうスカイツリー駅です");
+        assert_eq!("東京スカイツリー", tokens[0].text);
+        assert_eq!(
+            vec![
+                "名詞",
+                "固有名詞",
+                "一般",
+                "カスタム名詞",
+                "*",
+                "*",
+                "東京スカイツリー",
+                "トウキョウスカイツリー",
+                "トウキョウスカイツリー"
+            ],
+            tokens[0].detail
+        );
+        assert_eq!("とうきょうスカイツリー駅", tokens[4].text);
+        assert_eq!(
+            vec![
+                "カスタム名詞",
+                "*",
+                "*",
+                "*",
+                "*",
+                "*",
+                "とうきょうスカイツリー駅",
+                "トウキョウスカイツリーエキ",
+                "*"
+            ],
+            tokens[4].detail
+        );
+
         let token_texts: Vec<&str> = tokens.iter().map(|token| token.text).collect();
         assert_eq!(
             vec![
