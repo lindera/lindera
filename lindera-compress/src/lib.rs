f1f481a644e2469f2cf4f93d6e6501e10d8aba7e
higumachan <harekumo792154@gmail.com>
2021-12-27 23:16:59 +0900

    windowsでビルド出来るようにする

--- lindera-compress/src/lib.rs
+++ lindera-compress/src/lib.rs
@@ -1,22 +1,35 @@
 pub use lindera_decompress::{Algorithm, CompressedData};
-
+#[cfg(windows)]
+use lzma_rs::xz_decompress;
 
 #[allow(dead_code)]
 fn algorithm_compression_ratio_estimation() -> f64 {
     unimplemented!()
 }
 
+#[cfg(not(windows))]
 pub fn compress(data: &[u8], algorithm: Algorithm) -> anyhow::Result<CompressedData> {
     match algorithm {
         Algorithm::LZMA { preset } => {
-            /*
-            let mut buf_reader = BufReader::new(data);
-            let mut output_data = Vec::new();
-            lzma_compress(&mut buf_reader, &mut output_data)?;
-             */
             let output_data = lzma::compress(data, preset)?;
 
             Ok(CompressedData::new(algorithm, output_data))
+        }
+        Algorithm::Raw => Ok(CompressedData::new(algorithm, data.to_vec())),
+        _ => {
+            unimplemented!()
+        }
+    }
+}
+
+#[cfg(windows)]
+pub fn compress(data: &[u8], algorithm: Algorithm) -> anyhow::Result<CompressedData> {
+    match algorithm {
+        Algorithm::LZMA { preset } => {
+            let mut buf_reader = BufReader::new(data);
+            let mut output_data = Vec::new();
+            xz_compress(&mut buf_reader, &mut output_data)?;
+            Ok(output_data)
         }
         Algorithm::Raw => Ok(CompressedData::new(algorithm, data.to_vec())),
         _ => {
@@ -43,9 +56,7 @@ mod tests {
             buf.push(0)
         }
 
-        //dbg!(buf.len());
         let compress_data = compress(&buf, Algorithm::LZMA { preset: 9 }).unwrap();
-        //dbg!(compress_data.data.len());
 
         let data = decompress(compress_data).unwrap();
 
