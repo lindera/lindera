c6141f4036cf0c8e923cc9c60e456a6735aa2f17
Minoru Osuka <minoru.osuka@gmail.com>
2022-10-12 18:11:23 +0900

    Change HashSet to HashMap

--- lindera/src/character_filter/mapping.rs
+++ lindera/src/character_filter/mapping.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
+use yada::{builder::DoubleArrayBuilder, DoubleArray};
 
 use lindera_core::character_filter::CharacterFilter;
 
@@ -10,11 +11,11 @@ pub const MAPPING_CHARACTER_FILTER_NAME: &str = "mapping";
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct MappingCharacterFilterConfig {
-    pub mapping: HashMap<char, char>,
+    pub mapping: HashMap<String, String>,
 }
 
 impl MappingCharacterFilterConfig {
-    pub fn new(map: HashMap<char, char>) -> Self {
+    pub fn new(map: HashMap<String, String>) -> Self {
         Self { mapping: map }
     }
 
@@ -23,33 +24,73 @@ impl MappingCharacterFilterConfig {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct MappingCharacterFilter {
     config: MappingCharacterFilterConfig,
+    trie: DoubleArray<Vec<u8>>,
 }
 
 impl MappingCharacterFilter {
-    pub fn new(config: MappingCharacterFilterConfig) -> Self {
-        Self { config }
+    pub fn new(config: MappingCharacterFilterConfig) -> LinderaResult<Self> {
+        let mut keyset: Vec<(&[u8], u32)> = Vec::new();
+        let mut keys = config.mapping.keys().collect::<Vec<_>>();
+        keys.sort();
+        for (value, key) in keys.into_iter().enumerate() {
+            keyset.push((key.as_bytes(), value as u32));
+        }
+
+        let data = DoubleArrayBuilder::build(&keyset).ok_or_else(|| {
+            LinderaErrorKind::Io.with_error(anyhow::anyhow!("DoubleArray build error."))
+        })?;
+
+        let trie = DoubleArray::new(data);
+
+        Ok(Self { config, trie })
     }
 
     pub fn from_slice(data: &[u8]) -> LinderaResult<Self> {
-        Ok(Self::new(MappingCharacterFilterConfig::from_slice(data)?))
+        let config = MappingCharacterFilterConfig::from_slice(data)?;
+
+        Self::new(config)
     }
 }
 
 impl CharacterFilter for MappingCharacterFilter {
     fn apply(&self, text: &mut String) -> LinderaResult<()> {
-        *text = text
-            .chars()
-            .map(|c| {
-                if let Some(ch) = self.config.mapping.get(&c) {
-                    *ch
-                } else {
-                    c
+        let mut result = String::new();
+        let mut start = 0_usize;
+        let len = text.len();
+        while start < len {
+            let suffix = &text[start..];
+            match self
+                .trie
+                .common_prefix_search(suffix.as_bytes())
+                .last()
+                .map(|(_offset_len, prefix_len)| prefix_len)
+            {
+                Some(prefix_len) => {
+                    let surface = &text[start..start + prefix_len];
+                    let replacement = &self.config.mapping[surface];
+                    result.push_str(replacement);
+
+                    // move start offset
+                    start += prefix_len;
                 }
-            })
-            .collect::<String>();
+                None => {
+                    match suffix.chars().next() {
+                        Some(c) => {
+                            result.push(c);
+
+                            // move start offset
+                            start += c.len_utf8();
+                        }
+                        None => break,
+                    }
+                }
+            }
+        }
+
+        *text = result;
 
         Ok(())
     }
@@ -76,7 +117,7 @@ mod tests {
         "#;
         let config = MappingCharacterFilterConfig::from_slice(config_str.as_bytes()).unwrap();
 
-        assert_eq!(&'ア', config.mapping.get(&'ｱ').unwrap());
+        assert_eq!("ア", config.mapping.get("ｱ").unwrap());
     }
 
     #[test]
@@ -115,5 +156,33 @@ mod tests {
         let mut text = "ｱｲｳｴｵ".to_string();
         filter.apply(&mut text).unwrap();
         assert_eq!("アイウエオ", text);
+
+        let config_str = r#"
+        {
+            "mapping": {
+                "ﾘﾝﾃﾞﾗ": "リンデラ",
+                "リンデラ": "Lindera"
+            }
+        }
+        "#;
+        let filter = MappingCharacterFilter::from_slice(config_str.as_bytes()).unwrap();
+
+        let mut text = "ﾘﾝﾃﾞﾗ".to_string();
+        filter.apply(&mut text).unwrap();
+        assert_eq!("リンデラ", text);
+
+        let config_str = r#"
+        {
+            "mapping": {
+                "ﾘﾝﾃﾞﾗ": "リンデラ",
+                "リンデラ": "Lindera"
+            }
+        }
+        "#;
+        let filter = MappingCharacterFilter::from_slice(config_str.as_bytes()).unwrap();
+
+        let mut text = "Rust製形態素解析器ﾘﾝﾃﾞﾗで日本語を形態素解析する。".to_string();
+        filter.apply(&mut text).unwrap();
+        assert_eq!("Rust製形態素解析器リンデラで日本語を形態素解析する。", text);
     }
 }
