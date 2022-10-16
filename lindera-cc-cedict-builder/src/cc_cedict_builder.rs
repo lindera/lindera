e9808fef41c30523ff03bfef1b7bcb07a23f368b
Minoru Osuka <minoru.osuka@gmail.com>
2022-10-16 22:43:42 +0900

    Fix format

--- lindera-cc-cedict-builder/src/cc_cedict_builder.rs
+++ lindera-cc-cedict-builder/src/cc_cedict_builder.rs
@@ -279,7 +279,7 @@ impl DictionaryBuilder for CcCedictBuilder {
         let mut keyset: Vec<(&[u8], u32)> = vec![];
         for (key, word_entries) in &word_entry_map {
             let len = word_entries.len() as u32;
-            let val = (id << 5) | len;  // 27bit for word ID, 5bit for different parts of speech on the same surface.
+            let val = (id << 5) | len; // 27bit for word ID, 5bit for different parts of speech on the same surface.
             keyset.push((key.as_bytes(), val));
             id += len;
         }
