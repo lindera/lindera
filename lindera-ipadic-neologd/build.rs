e0c6dbae30d404bad61dcdad6a45523c787bef0d
Minoru Osuka <minoru.osuka@gmail.com>
2023-05-24 22:39:13 +0900

    Download NEologd dictionary from Google Drive

--- lindera-ipadic-neologd/build.rs
+++ lindera-ipadic-neologd/build.rs
@@ -4,8 +4,8 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     use std::{
         env,
-        fs::{create_dir, File},
-        io::{Cursor, Read, Write},
+        fs::{create_dir, rename, File},
+        io::{self, Cursor, Read, Write},
         path::Path,
     };
 
@@ -24,6 +24,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Directory path for build package
     let build_dir = env::var_os("OUT_DIR").unwrap(); // ex) target/debug/build/<pkg>/out
+
+    // Dictionary file name
+    let file_name = "mecab-ipadic-2.7.0-20070801-neologd-20200820.tar.gz";
 
     // MeCab IPADIC directory
     let input_dir = Path::new(&build_dir).join("mecab-ipadic-2.7.0-20070801-neologd-20200820");
@@ -52,17 +55,28 @@ fn main() -> Result<(), Box<dyn Error>> {
         let mut dummy_matrix_def = File::create(input_dir.join("matrix.def"))?;
         dummy_matrix_def.write_all(b"0 1 0\n")?;
     } else {
-        // Resources directory
-        let resources_dir_path = Path::new("resources");
+        // Source file path for build package
+        let source_path_for_build = Path::new(&build_dir).join(file_name);
 
-        // Dictionary file name
-        let dict_file_name = "mecab-ipadic-2.7.0-20070801-neologd-20200820.tar.gz";
+        // Download source file to build directory
+        if !source_path_for_build.exists() {
+            // copy(&source_path, &source_path_for_build)?;
+            let tmp_path = Path::new(&build_dir).join(file_name.to_owned() + ".download");
 
-        // Source dictionary file path
-        let source_dict_file_path = resources_dir_path.join(dict_file_name);
+            // Download a tarball
+            let download_url =
+                "https://drive.google.com/uc?export=download&confirm=yes&id=1zZeh16ezz5z6I40HaynHeit4EQsyDuPQ";
+            let resp = ureq::get(download_url).call()?;
+            let mut dest = File::create(&tmp_path)?;
 
+            io::copy(&mut resp.into_reader(), &mut dest)?;
+            dest.flush()?;
+
+            rename(tmp_path, &source_path_for_build).expect("Failed to rename temporary file");
+        }
+        
         // Decompress a tar.gz file
-        let mut tar_gz = File::open(source_dict_file_path)?;
+        let mut tar_gz = File::open(source_path_for_build)?;
         let mut buffer = Vec::new();
         tar_gz.read_to_end(&mut buffer)?;
         let cursor = Cursor::new(buffer);
