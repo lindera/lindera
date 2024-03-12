614aa5afc424cf6f3f20332373afc8edd945f42a
Minoru Osuka <minoru.osuka@gmail.com>
2024-03-12 13:46:59 +0900

    Download UniDic from self repository (#379)
    * Download UniDic from self repository
    
    * Delete unused dependency

--- lindera-unidic/build.rs
+++ lindera-unidic/build.rs
@@ -2,18 +2,15 @@ use std::error::Error;
 
 #[cfg(feature = "unidic")]
 fn main() -> Result<(), Box<dyn Error>> {
-    use std::{
-        env,
-        fs::{self, create_dir, rename, File},
-        io::{self, Write},
-        path::Path,
-    };
+    use std::env;
+    use std::fs::{create_dir, rename, File};
+    use std::io::{self, Cursor, Read, Write};
+    use std::path::Path;
 
-    use encoding::{
-        all::UTF_8,
-        {EncoderTrap, Encoding},
-    };
-    use zip::ZipArchive;
+    use encoding::all::UTF_8;
+    use encoding::{EncoderTrap, Encoding};
+    use flate2::read::GzDecoder;
+    use tar::Archive;
 
     use lindera_core::dictionary_builder::DictionaryBuilder;
     use lindera_unidic_builder::unidic_builder::UnidicBuilder;
@@ -25,10 +22,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let build_dir = env::var_os("OUT_DIR").unwrap(); // ex) target/debug/build/<pkg>/out
 
     // Dictionary file name
-    let file_name = "unidic-mecab-2.1.2_src.zip";
+    let file_name = "unidic-mecab-2.1.2.tar.gz";
 
     // UniDic MeCab directory
-    let input_dir = Path::new(&build_dir).join("unidic-mecab-2.1.2_src");
+    let input_dir = Path::new(&build_dir).join("unidic-mecab-2.1.2");
 
     // Lindera IPADIC directory
     let output_dir = Path::new(&build_dir).join("lindera-unidic");
@@ -37,9 +34,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         // Use dummy data in docs.rs.
         create_dir(&input_dir)?;
 
+        // Create dummy char.def
         let mut dummy_char_def = File::create(input_dir.join("char.def"))?;
         dummy_char_def.write_all(b"DEFAULT 0 1 0\n")?;
 
+        // Create dummy CSV file
         let mut dummy_dict_csv = File::create(input_dir.join("dummy_dict.csv"))?;
         dummy_dict_csv
             .write_all(
@@ -51,6 +50,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     .unwrap(),
             )?;
 
+        // Create dummy unk.def
         File::create(input_dir.join("unk.def"))?;
         let mut dummy_matrix_def = File::create(input_dir.join("matrix.def"))?;
         dummy_matrix_def.write_all(b"0 1 0\n")?;
@@ -64,7 +64,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             let tmp_path = Path::new(&build_dir).join(file_name.to_owned() + ".download");
 
             // Download a tarball
-            let download_url = "https://download.johtani.info/unidic-mecab-2.1.2_src.zip";
+            let download_url =
+                "https://github.com/lindera-morphology/unidic-mecab/archive/refs/tags/2.1.2.tar.gz";
             let resp = ureq::get(download_url).call()?;
             let mut dest = File::create(&tmp_path)?;
 
@@ -74,49 +75,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             rename(tmp_path, &source_path_for_build).expect("Failed to rename temporary file");
         }
 
-        // Unzip
-        let zip_file = File::open(&source_path_for_build)?;
-        let mut archive = ZipArchive::new(zip_file)?;
-        for i in 0..archive.len() {
-            let mut file = archive.by_index(i)?;
-            let output_file_path = match file.enclosed_name() {
-                Some(path) => Path::new(&build_dir).join(path),
-                None => continue,
-            };
-            {
-                let comment = file.comment();
-                if !comment.is_empty() {
-                    println!("File {} comment: {}", i, comment);
-                }
-            }
-            if (*file.name()).ends_with('/') {
-                println!("File {} extracted to \"{}\"", i, output_file_path.display());
-                fs::create_dir_all(&output_file_path)?;
-            } else {
-                println!(
-                    "File {} extracted to \"{}\" ({} bytes)",
-                    i,
-                    output_file_path.display(),
-                    file.size()
-                );
-                if let Some(p) = output_file_path.parent() {
-                    if !p.exists() {
-                        fs::create_dir_all(p)?;
-                    }
-                }
-                let mut outfile = fs::File::create(&output_file_path)?;
-                io::copy(&mut file, &mut outfile)?;
-            }
-            // Get and Set permissions
-            #[cfg(unix)]
-            {
-                use std::os::unix::fs::PermissionsExt;
-
-                if let Some(mode) = file.unix_mode() {
-                    fs::set_permissions(&output_file_path, fs::Permissions::from_mode(mode))?;
-                }
-            }
-        }
+        // Decompress a tar.gz file
+        let mut tar_gz = File::open(source_path_for_build)?;
+        let mut buffer = Vec::new();
+        tar_gz.read_to_end(&mut buffer)?;
+        let cursor = Cursor::new(buffer);
+        let decoder = GzDecoder::new(cursor);
+        let mut archive = Archive::new(decoder);
+        archive.unpack(&build_dir)?;
     }
 
     // Build a dictionary
