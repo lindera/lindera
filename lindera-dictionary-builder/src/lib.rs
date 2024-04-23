810ce025d662ed12d2ba529f38d8275a89c7944b
Yoonchae Lee <bluegreenmagick@gmail.com>
2024-04-23 19:03:04 +0900

    cleanup root namepsace exports

--- lindera-dictionary-builder/src/lib.rs
+++ lindera-dictionary-builder/src/lib.rs
@@ -11,10 +11,8 @@ pub mod unk;
 pub mod user_dict;
 pub mod utils;
 
-pub use chardef::{CharDefBuilder, CharDefBuilderOptions, CharDefBuilderOptionsError};
-pub use cost_matrix::{CostMatrixBuilder, CostMatrixBuilderOptions, CostMatrixBuilderOptionsError};
-pub use dict::{DictBuilder, DictBuilderOptions, DictBuilderOptionsError};
-pub use unk::{UnkBuilder, UnkBuilderOptions, UnkBuilderOptionsError};
-pub use user_dict::{
-    build_user_dictionary, UserDictBuilder, UserDictBuilderOptions, UserDictBuilderOptionsError,
-};
+pub use chardef::CharDefBuilderOptions;
+pub use cost_matrix::CostMatrixBuilderOptions;
+pub use dict::DictBuilderOptions;
+pub use unk::UnkBuilderOptions;
+pub use user_dict::{build_user_dictionary, UserDictBuilderOptions};
