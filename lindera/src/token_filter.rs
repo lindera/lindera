731724ca0b362870b41da08f9dfc2a8e9f6b262c
Minoru Osuka <minoru.osuka@gmail.com>
2024-09-22 10:56:31 +0900

    Add docs

--- lindera/src/token_filter.rs
+++ lindera/src/token_filter.rs
@@ -1,3 +1,36 @@
+/// This module defines various token filters and provides functionality to load them.
+///
+/// # Modules
+/// - `japanese_base_form`: Contains the Japanese base form token filter.
+/// - `japanese_compound_word`: Contains the Japanese compound word token filter.
+/// - `japanese_kana`: Contains the Japanese kana token filter.
+/// - `japanese_katakana_stem`: Contains the Japanese katakana stem token filter.
+/// - `japanese_keep_tags`: Contains the Japanese keep tags token filter.
+/// - `japanese_number`: Contains the Japanese number token filter.
+/// - `japanese_reading_form`: Contains the Japanese reading form token filter.
+/// - `japanese_stop_tags`: Contains the Japanese stop tags token filter.
+/// - `keep_words`: Contains the keep words token filter.
+/// - `korean_keep_tags`: Contains the Korean keep tags token filter.
+/// - `korean_reading_form`: Contains the Korean reading form token filter.
+/// - `korean_stop_tags`: Contains the Korean stop tags token filter.
+/// - `length`: Contains the length token filter.
+/// - `lowercase`: Contains the lowercase token filter.
+/// - `mapping`: Contains the mapping token filter.
+/// - `stop_words`: Contains the stop words token filter.
+/// - `uppercase`: Contains the uppercase token filter.
+///
+/// # Traits
+/// - `TokenFilter`: A trait for token filters that can be applied to a vector of tokens.
+/// - `TokenFilterClone`: A trait for cloning boxed token filters.
+///
+/// # Structs
+/// - `BoxTokenFilter`: A boxed token filter that implements `TokenFilter`.
+/// - `TokenFilterLoader`: A loader for creating token filters from configuration values.
+///
+/// # Usage
+/// The `TokenFilterLoader` struct provides methods to load token filters from configuration values
+/// or command-line flags. The `TokenFilter` trait defines the interface for token filters, and
+/// `BoxTokenFilter` is a boxed implementation of a token filter.
 pub mod japanese_base_form;
 pub mod japanese_compound_word;
 pub mod japanese_kana;
@@ -76,12 +109,28 @@ use crate::token_filter::stop_words::{
 use crate::token_filter::uppercase::{UppercaseTokenFilter, UPPERCASE_TOKEN_FILTER_NAME};
 use crate::{LinderaErrorKind, LinderaResult};
 
+/// A trait for token filters that can be applied to a vector of tokens.
+///
+/// This trait requires the implementor to be `'static`, `Send`, `Sync`, and
+/// implement the `TokenFilterClone` trait. It provides methods to get the
+/// name of the filter and to apply the filter to a mutable vector of tokens.
+///
+/// # Required Methods
+///
+/// - `name`: Returns the name of the token filter as a static string slice.
+/// - `apply`: Applies the token filter to a mutable vector of tokens, returning
+///   a `LinderaResult<()>`.
 pub trait TokenFilter: 'static + Send + Sync + TokenFilterClone {
     fn name(&self) -> &'static str;
     // fn apply<'a>(&self, tokens: &mut Vec<Token<'a>>) -> LinderaResult<()>;
     fn apply(&self, tokens: &mut Vec<Token<'_>>) -> LinderaResult<()>;
 }
 
+/// A `BoxTokenFilter` is a wrapper around a boxed trait object that implements
+/// the `TokenFilter` trait. This allows for dynamic dispatch of different
+/// `TokenFilter` implementations at runtime. The `BoxTokenFilter` ensures that
+/// the contained `TokenFilter` is thread-safe (`Send` and `Sync`) and has a
+/// static lifetime.
 pub struct BoxTokenFilter(Box<dyn TokenFilter + 'static + Send + Sync>);
 
 impl Deref for BoxTokenFilter {
@@ -98,6 +147,10 @@ impl<T: TokenFilter> From<T> for BoxTokenFilter {
     }
 }
 
+/// A trait for cloning token filters.
+///
+/// This trait provides a method `box_clone` which allows for cloning
+/// a token filter and returning it as a boxed trait object.
 pub trait TokenFilterClone {
     fn box_clone(&self) -> BoxTokenFilter;
 }
@@ -112,6 +165,24 @@ pub struct TokenFilterLoader {}
 
 impl TokenFilterLoader {
     pub fn load_from_value(kind: &str, value: &Value) -> LinderaResult<BoxTokenFilter> {
+        // Creates a `BoxTokenFilter` based on the provided `kind` and `value`.
+        //
+        // The function matches the `kind` against various predefined token filter names
+        // and constructs the corresponding token filter using the configuration derived
+        // from `value`. If the `kind` does not match any of the predefined names, an error
+        // is returned.
+        //
+        // # Parameters
+        // - `kind`: A string slice that specifies the type of token filter to create.
+        // - `value`: A `serde_json::Value` that contains the configuration for the token filter.
+        //
+        // # Returns
+        // - `Result<BoxTokenFilter, LinderaError>`: A boxed token filter if the `kind` is recognized,
+        //   otherwise an error indicating that the token filter is unsupported.
+        //
+        // # Errors
+        // - Returns `LinderaErrorKind::Deserialize` if the `kind` is not supported or if there is an
+        //   error in creating the token filter from the provided `value`.
         let token_filter = match kind {
             JAPANESE_BASE_FORM_TOKEN_FILTER_NAME => {
                 let config = JapaneseBaseFormTokenFilterConfig::from_value(value)?;
@@ -183,6 +254,31 @@ impl TokenFilterLoader {
         Ok(token_filter)
     }
 
+    /// Loads a token filter based on a CLI flag string.
+    ///
+    /// # Arguments
+    ///
+    /// * `cli_flag` - A string slice representing the command-line interface (CLI) flag used to specify the token filter. The flag typically contains both the filter kind and its arguments.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `LinderaResult<BoxTokenFilter>`, which is a boxed token filter, or an error if the CLI flag is invalid or the filter configuration cannot be loaded.
+    ///
+    /// # Process
+    ///
+    /// 1. **Parse CLI flag**:
+    ///    - The `parse_cli_flag` function is called to extract the filter kind and its arguments from the `cli_flag` string.
+    /// 2. **Load filter from parsed values**:
+    ///    - The filter kind and arguments are passed to `load_from_value`, which constructs the appropriate token filter based on the parsed values.
+    ///
+    /// # Errors
+    ///
+    /// - If the CLI flag cannot be parsed, an error is returned.
+    /// - If the filter kind or its configuration is invalid, an error is returned during the filter loading process.
+    ///
+    /// # Details
+    ///
+    /// - The CLI flag is parsed into a filter kind and arguments. These are then used to load the appropriate token filter using the `load_from_value` function.
     pub fn load_from_cli_flag(cli_flag: &str) -> LinderaResult<BoxTokenFilter> {
         let (kind, args) = parse_cli_flag(cli_flag)?;
 
