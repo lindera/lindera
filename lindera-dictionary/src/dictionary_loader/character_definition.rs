7d9295e60d0bb55bbc489a06a72c45afdd462281
Minoru Osuka <minoru.osuka@gmail.com>
2024-11-02 22:41:15 +0900

    Fix dictionary loader

--- lindera-dictionary/src/dictionary_loader/character_definition.rs
+++ lindera-dictionary/src/dictionary_loader/character_definition.rs
@@ -1,15 +1,27 @@
 use std::path::Path;
 
+#[cfg(feature = "compress")]
+use crate::decompress::decompress;
 use crate::dictionary::character_definition::CharacterDefinition;
+#[cfg(feature = "compress")]
+use crate::error::LinderaErrorKind;
 use crate::util::read_file;
 use crate::LinderaResult;
 
 pub struct CharacterDefinitionLoader {}
 
 impl CharacterDefinitionLoader {
+    #[allow(unused_mut)]
     pub fn load(input_dir: &Path) -> LinderaResult<CharacterDefinition> {
-        let path = input_dir.join("char_def.bin");
-        let data = read_file(path.as_path())?;
+        let mut data = read_file(input_dir.join("char_def.bin").as_path())?;
+
+        #[cfg(feature = "compress")]
+        {
+            let compressed_data = bincode::deserialize_from(data.as_slice())
+                .map_err(|err| LinderaErrorKind::Deserialize.with_error(err))?;
+            data = decompress(compressed_data)
+                .map_err(|err| LinderaErrorKind::Decompress.with_error(err))?;
+        }
 
         CharacterDefinition::load(data.as_slice())
     }
