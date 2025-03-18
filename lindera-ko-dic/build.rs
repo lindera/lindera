e58ba108f63f6f878ebae6572310b237524617f3
Minoru OSUKA <minoru.osuka@gmail.com>
2025-03-18 16:34:31 +0900

    Change dictionary download URL (#462)

--- lindera-ko-dic/build.rs
+++ lindera-ko-dic/build.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             file_name: "mecab-ko-dic-2.1.1-20180720.tar.gz",
             input_dir: "mecab-ko-dic-2.1.1-20180720",
             output_dir: "lindera-ko-dic",
-            download_url: "https://dlwqk3ibdg1xh.cloudfront.net/mecab-ko-dic-2.1.1-20180720.tar.gz",
+            download_url: "https://lindera.s3.ap-northeast-1.amazonaws.com/mecab-ko-dic-2.1.1-20180720.tar.gz",
             dummy_input: "테스트,1785,3543,4721,NNG,행위,F,테스트,*,*,*,*\n",
         },
         lindera_dictionary::dictionary_builder::ko_dic::KoDicBuilder::new(),
