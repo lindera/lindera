248a2f0bd9dfbf74332e7e931d2655d457d8125d
Minoru OSUKA <minoru.osuka@gmail.com>
2025-03-18 23:35:22 +0900

    Change dictionary download URL (#464)

--- lindera-ipadic-neologd/build.rs
+++ lindera-ipadic-neologd/build.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             input_dir: "mecab-ipadic-neologd-0.0.7-20200820",
             output_dir: "lindera-ipadic-neologd",
             download_url:
-                "https://dlwqk3ibdg1xh.cloudfront.net/mecab-ipadic-neologd-0.0.7-20200820.tar.gz",
+                "https://lindera.s3.ap-northeast-1.amazonaws.com/mecab-ipadic-neologd-0.0.7-20200820.tar.gz",
             dummy_input: "テスト,1288,1288,-1000,名詞,固有名詞,一般,*,*,*,*,*,*\n",
         },
         lindera_ipadic_neologd_builder::ipadic_neologd_builder::IpadicNeologdBuilder::new(),
--- lindera-ipadic/build.rs
+++ lindera-ipadic/build.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             file_name: "mecab-ipadic-2.7.0-20070801.tar.gz",
             input_dir: "mecab-ipadic-2.7.0-20070801",
             output_dir: "lindera-ipadic",
-            download_url: "https://dlwqk3ibdg1xh.cloudfront.net/mecab-ipadic-2.7.0-20070801.tar.gz",
+            download_url: "https://lindera.s3.ap-northeast-1.amazonaws.com/mecab-ipadic-2.7.0-20070801.tar.gz",
             dummy_input: "テスト,1288,1288,-1000,名詞,固有名詞,一般,*,*,*,*,*,*\n",
         },
         lindera_ipadic_builder::ipadic_builder::IpadicBuilder::new(),
