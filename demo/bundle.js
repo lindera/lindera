043ccfee1a32471daf5d817871c078f9354535e0
mosuka <mosuka@users.noreply.github.com>
2026-09-23 11:51:25 +0000

    deploy: 72cd237090c932d4cf8ed523c25a6352a178eaa5

--- demo/bundle.js
+++ demo/bundle.js
@@ -26,7 +26,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pk
   \***********************************/
 (module, __unused_webpack_exports, __webpack_require__) {
 
-eval("{module.exports = __webpack_require__.p + \"4eb2cd1add4b2f7eba93.wasm\";\n\n//# sourceURL=webpack://lindera-wasm-example/../pkg/lindera_wasm_bg.wasm?\n}");
+eval("{module.exports = __webpack_require__.p + \"722b7c6efb6176833a6d.wasm\";\n\n//# sourceURL=webpack://lindera-wasm-example/../pkg/lindera_wasm_bg.wasm?\n}");
 
 /***/ },
 
